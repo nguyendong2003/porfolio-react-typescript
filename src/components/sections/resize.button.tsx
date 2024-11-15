e378c820a0387ab7494226df0e342dedbdb83e81
Nguyen Dong <dongcoi14122003@gmail.com>
2024-11-16 00:01:44 +0700

    migrate hero section to tsx in home page

--- src/components/sections/resize.button.tsx
+++ src/components/sections/resize.button.tsx
@@ -1,5 +1,11 @@
 
-const ResizeButton = (props) => {
+interface IProps {
+    btnText?: string;
+    btnIcons?: React.ReactNode;
+    btnStyle?: React.CSSProperties;  // CSSProperties được gợi ý css code ở props truyền vào
+}
+
+const ResizeButton = (props: IProps) => {
     const { btnText, btnIcons, btnStyle } = props;
 
     return (
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+
+// This will allow you to import image files in your TypeScript code without any error.
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
